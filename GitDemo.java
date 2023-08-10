fc121e5f166bac38d4e06ec5cefb70be6b7133e3
NagarajuJN <nagaraju1998jn@gmail.com>
2023-08-10 15:43:26 +0530

    For loop added

--- GitDemo.java
+++ GitDemo.java
@@ -2,6 +2,8 @@ class GitDemo
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("NEVER GIVE-UP");
+		for(int i=0;i<=10;i++){
+			System.out.println("Welcome to Pentagon Space");
+		}
 	}
 }
